7d3c5cc25ddef2c7de017a861a53624892bd40df
theonlysaimon <89244188+theonlysaimon@users.noreply.github.com>
2021-08-20 17:45:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print ("Привет мир gggggggg!")
-print ("Я кого-то ебал!")
+import platform,socket,re,uuid,json
+import psutil
+
+def getSystemInfo():
+	try:
+		info={}
+		info['platform']=platform.system()
+		info['platform-release']=platform.release()
+		info['platform-version']=platform.version()
+		info['architecture']=platform.machine()
+		info['hostname']=socket.gethostname()
+		info['ip-address']=socket.gethostbyname(socket.gethostname())
+		info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+		info['processor']=platform.processor()
+		info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+		return json.dumps(info)
+	except Exception as e:
+		logging.exception(e)
+
+print(getSystemInfo())
+
+
+
